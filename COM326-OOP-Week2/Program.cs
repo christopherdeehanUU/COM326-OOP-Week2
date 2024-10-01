a35199f60947d32c60f7c07b97510ed318d80725
christopherdeehanUU <deehan-c7@ulster.ac.uk>
2024-10-01 11:15:26 +0100

    Update Program.cs
    Q2 done

--- COM326-OOP-Week2/Program.cs
+++ COM326-OOP-Week2/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 
 class Program
 {
-    //Q1
     static void PrintMenu()
     {
         Console.WriteLine("Please enter a valid option from below:");
@@ -13,32 +12,58 @@ class Program
         Console.WriteLine("0. Exit application");
     }
 
+    static int InputOption()
+    {
+        string input = Console.ReadLine();
+
+        try
+        {
+            int option = int.Parse(input);
+            return option;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Input is not a valid number.");
+            return -1;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Number entered is too small or too large.");
+            return -1;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occured: {ex.Message}");
+            return -1;
+        }
+
+    }
 
     static void Main(string[] args)
     {
         PrintMenu();
 
-        string userInput = Console.ReadLine();
+        int option = InputOption();
 
-        switch (userInput)
+        switch (option)
         {
-            case "1":
+            case 1:
                 Console.WriteLine("Hello in French: Bonjour");
                 break;
-            case "2":
+            case 2:
                 Console.WriteLine("Hello in Spanish: Hola");
                 break;
-            case "3":
+            case 3:
                 Console.WriteLine("Hello in German: Hallo");
                 break;
-            case "4":
+            case 4:
                 Console.WriteLine("Hello in Italian: Ciao");
                 break;
-            case "0":
+            case 0:
                 Console.WriteLine("Exiting application");
                 break;
             default:
-                Console.WriteLine("Error. Try again");
+                Console.WriteLine("Error, Try again.");
                 break;
 
         }
